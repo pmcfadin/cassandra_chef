993513687b7de96113263ed6f72e02f66a19575c
Patrick McFadin <pmcfadin@gmail.com>
2011-10-08 09:39:41 -0700

    Changed normal attributes to default to get around RightScale problem using Chef .8

--- cookbooks/brisk/attributes/default.rb
+++ cookbooks/brisk/attributes/default.rb
@@ -1,25 +1,25 @@
 # Needed for the Chef script to function properly
-normal[:setup][:cluster_size] = 4
-normal[:setup][:current_role] = "brisk"
+default[:setup][:cluster_size] = 4
+default[:setup][:current_role] = "brisk"
 
 # A unique name is preferred to stop the risk of different clusters joining each other
-normal[:cassandra][:cluster_name] = "Brisk Cluster"
+default[:cassandra][:cluster_name] = "Brisk Cluster"
 
 # It is best to have the commit log and the data
 # directory on two seperate drives
-normal[:cassandra][:commitlog_dir] = "/var/lib"
-normal[:cassandra][:data_dir] = "/var/lib"
+default[:cassandra][:commitlog_dir] = "/var/lib"
+default[:cassandra][:data_dir] = "/var/lib"
 
 # The number of non-task trackers you wish to run
 default[:brisk][:vanilla_nodes] = 2
 
 
 # Advanced Cassandra settings
-normal[:cassandra][:token_position] = false
-normal[:cassandra][:initial_token] = false
-normal[:cassandra][:seed] = false
-normal[:cassandra][:rpc_address] = false
-normal[:cassandra][:confPath] = "/etc/brisk/cassandra/"
+default[:cassandra][:token_position] = false
+default[:cassandra][:initial_token] = false
+default[:cassandra][:seed] = false
+default[:cassandra][:rpc_address] = false
+default[:cassandra][:confPath] = "/etc/brisk/cassandra/"
 
 # Advanced Brisk settings
 default[:brisk][:endpoint_snitch] = "org.apache.cassandra.locator.BriskSimpleSnitch"
