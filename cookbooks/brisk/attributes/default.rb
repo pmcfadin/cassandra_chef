eb181f0418e8206b840c0eb8887cceec6d523ebf
Patrick McFadin <pmcfadin@gmail.com>
2011-10-08 21:12:48 -0700

    Setting back to normal

--- cookbooks/brisk/attributes/default.rb
+++ cookbooks/brisk/attributes/default.rb
@@ -1,25 +1,25 @@
 # Needed for the Chef script to function properly
-set[:setup][:cluster_size] = 4
-set[:setup][:current_role] = "brisk"
+normal[:setup][:cluster_size] = 4
+normal[:setup][:current_role] = "brisk"
 
 # A unique name is preferred to stop the risk of different clusters joining each other
-set[:cassandra][:cluster_name] = "Brisk Cluster"
+normal[:cassandra][:cluster_name] = "Brisk Cluster"
 
 # It is best to have the commit log and the data
 # directory on two seperate drives
-set[:cassandra][:commitlog_dir] = "/var/lib"
-set[:cassandra][:data_dir] = "/var/lib"
+normal[:cassandra][:commitlog_dir] = "/var/lib"
+normal[:cassandra][:data_dir] = "/var/lib"
 
 # The number of non-task trackers you wish to run
 default[:brisk][:vanilla_nodes] = 2
 
 
 # Advanced Cassandra settings
-set[:cassandra][:token_position] = false
-set[:cassandra][:initial_token] = false
-set[:cassandra][:seed] = false
-set[:cassandra][:rpc_address] = false
-set[:cassandra][:confPath] = "/etc/brisk/cassandra/"
+normal[:cassandra][:token_position] = false
+normal[:cassandra][:initial_token] = false
+normal[:cassandra][:seed] = false
+normal[:cassandra][:rpc_address] = false
+normal[:cassandra][:confPath] = "/etc/brisk/cassandra/"
 
 # Advanced Brisk settings
-set[:brisk][:endpoint_snitch] = "org.apache.cassandra.locator.BriskSimpleSnitch"
+normal[:brisk][:endpoint_snitch] = "org.apache.cassandra.locator.BriskSimpleSnitch"
